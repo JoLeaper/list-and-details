ca7c730618afd970ad34d6df1b1b4b2ed2630240
JoLeaper <joleap123@gmail.com>
2020-07-28 13:03:47 -0700

    added customhook

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,37 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ListPage from '../components/ListPage/ListPage';
 import SearchBar from '../components/SearchBar/SearchBar';
-import getAllCharacters from '../services/getAllCharacters';
+import { useCharacters } from '../hooks/useCharactersHooks.js'; 
 
 const MainContainer = () => {
-  const [characters, setCharacters] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  useEffect(() => {
-    getAllCharacters()
-      .then(initialCharacters => setCharacters(initialCharacters));
-  }, []);
-  
-  const handleChange = ({ target }) => {
-    switch(target.name) {
-      case 'characters':
-        setCharacters(target.value);
-        break;
-      case 'searchQuery':
-        setSearchQuery(target.value);
-        break;
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchNewSearch(searchQuery).then(characters => {
-      setCharacters(characters);
-    });
-    
-  };
-
-
+  const { 
+    characters, 
+    searchQuery, 
+    handleChange, 
+    handleSubmit } = useCharacters();
 
   return (
     <div>
---
+++ src/hooks/useCharactersHooks.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'React';
+import  getAllCharacters  from '../services/getAllCharacters';
+import  fetchNewSearch  from '../services/fetchNewSearch';
+
+export const useCharacters = () => {
+  const [characters, setCharacters] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    getAllCharacters()
+      .then(initialCharacters => setCharacters(initialCharacters));
+  }, []);
+
+  const handleChange = ({ target }) => {
+    switch(target.name) {
+      case 'characters':
+        setCharacters(target.value);
+        break;
+      case 'searchQuery':
+        setSearchQuery(target.value);
+        break;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchNewSearch(searchQuery)
+      .then(characters => {
+        setCharacters(characters);
+      });
+  
+  };
+
+  return {
+    characters,
+    searchQuery,
+    handleChange,
+    handleSubmit
+  };
+};
+
